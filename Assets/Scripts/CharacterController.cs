e6932732833f4e468b04fd8469ce8471c5183487
Sebp23 <57201896+Sebp23@users.noreply.github.com>
2020-10-07 15:24:36 -0500

    Added ability for sprite to flip (will likely edit but works for now)

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -10,15 +10,19 @@ public class CharacterController : MonoBehaviour
     [SerializeField]
     private float jumpHeight = 5f;
 
+    private bool facingRight = true;
+
     public bool playerOnGround = false;
 
     private Rigidbody2D rigidbody;
+    private SpriteRenderer playerSpriteRenderer;
     private Vector2 input;
     
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        playerSpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void FixedUpdate()
@@ -33,6 +37,7 @@ public class CharacterController : MonoBehaviour
     {
         input.x = Input.GetAxisRaw("Horizontal");
         input.y = Input.GetAxisRaw("Vertical");
+        FaceMouse();
     }
 
     void Jump()
@@ -42,6 +47,25 @@ public class CharacterController : MonoBehaviour
         if (Input.GetKey(KeyCode.W) && playerOnGround)
         {
             rigidbody.AddForce(jumpVector, ForceMode2D.Impulse);
+        }
+    }
+
+    void FaceMouse()
+    {
+        //Credit: https://answers.unity.com/questions/1604524/character-facing-the-position-of-mouse-cursor-2d-p.html
+
+        // using mousePosition and player's transform (on orthographic camera view)
+        var delta = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+
+        if (delta.x >= 0 && !facingRight)
+        { // mouse is on right side of player
+            transform.localScale = new Vector3(1, 1, 1); // or activate look right some other way
+            facingRight = true;
+        }
+        else if (delta.x < 0 && facingRight)
+        { // mouse is on left side
+            transform.localScale = new Vector3(-1, 1, 1); // activate looking left
+            facingRight = false;
         }
     }
 }
