181778e44ffa3476e5a09a758bcd425a49dff799
Sebp23 <57201896+Sebp23@users.noreply.github.com>
2020-10-07 14:34:50 -0500

    Added jumping and x-axis movement

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField]
+    private float maxSpeed = 5f;
+
+    [SerializeField]
+    private float jumpHeight = 5f;
+
+    [SerializeField]
+    private bool onGround = false;
+
+    private Vector2 input;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(input.x, 0, 0);
+        transform.position += movement * Time.deltaTime * maxSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+        Jump();
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.W) && onGround)
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpHeight), ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            onGround = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            onGround = false;
+        }
+    }
+}
