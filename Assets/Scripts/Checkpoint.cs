a5339cef148f10828def71600b957f1113bda664
Sebp23 <57201896+Sebp23@users.noreply.github.com>
2020-11-16 15:47:58 -0600

    Windmill checkpoint now rotates when player passes (activates) it

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -4,19 +4,34 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
+    [SerializeField]
+    private float rotateSpeed;
+
     private GameMaster gm;
+    private GameObject windmillBlades;
+    private bool checkpointActivated = false;
 
     private void Start()
     {
         gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        windmillBlades = transform.Find("Windmill Blades").gameObject;
+    }
+
+    private void Update()
+    {
+        if (checkpointActivated)
+        {
+            windmillBlades.transform.Rotate(Vector3.back * Time.deltaTime * rotateSpeed);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            //the respawn position is equal to checkpoint position once player passes checkpoint.s
+            //the respawn position is equal to checkpoint position once player passes checkpoint.
             gm.lastCheckPointPosition = gameObject.transform.position;
+            checkpointActivated = true;
         }
     }
 }
