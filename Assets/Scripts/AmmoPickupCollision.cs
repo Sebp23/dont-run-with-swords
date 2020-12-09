17f5478446813681123995ca27f60d115175f803
Sebp23 <57201896+Sebp23@users.noreply.github.com>
2020-12-08 18:53:21 -0800

    Added ammo pickup sound and checkpoint sound

--- Assets/Scripts/AmmoPickupCollision.cs
+++ Assets/Scripts/AmmoPickupCollision.cs
@@ -6,15 +6,19 @@ public class AmmoPickupCollision : MonoBehaviour
 {
     [SerializeField]
     private float pickupAmount = 1;
+    [SerializeField]
+    private AudioClip ammoPickupSound;
 
     private bool ammoPickedUp = false;
 
     private Ammo ammoScript;
+    private AudioSource playerAudio;
 
     // Start is called before the first frame update
     void Start()
     {
         ammoScript = GameObject.Find("Ammo Message").GetComponent<Ammo>();
+        playerAudio = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -24,11 +28,12 @@ public class AmmoPickupCollision : MonoBehaviour
             Debug.Log("Ammo Detected");
             if (ammoScript.currentAmmo < ammoScript.maxAmmo)
             {
+                playerAudio.PlayOneShot(ammoPickupSound);
                 ammoScript.currentAmmo += pickupAmount;
                 Debug.Log(pickupAmount);
-            }
 
-            Destroy(collision.gameObject);
+                Destroy(collision.gameObject);
+            }
         }
     }
 }
