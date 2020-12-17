87186375a6d36909555ce6ceb4fc593a89116341
Sebp23 <57201896+Sebp23@users.noreply.github.com>
2020-12-17 13:40:32 -0800

    Cleaned up code in EnemyController

--- Assets/Scripts/Controllers/EnemyController.cs
+++ Assets/Scripts/Controllers/EnemyController.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     //Code tutorial/source for some of this script: https://www.youtube.com/watch?v=aRxuKoJH9Y0&ab_channel=Blackthornprod
-
+    [Tooltip("How fast the enemy moves")]
     [SerializeField]
     private float speed;
+
+    [Tooltip("How far away the enemy detects the collider so they don't fall off the platform")]
     [SerializeField]
-    private float distance;
+    private float platformDistance;
+
+    [Tooltip("How far away the enemy detects the player")]
     [SerializeField]
     private float detectDistance;
 
@@ -17,6 +21,7 @@ public class EnemyController : MonoBehaviour
     private bool facingRight = true;
     public bool playerDetected = false;
 
+    [Tooltip("An empty object attached to the enemy object that detects the ground")]
     [SerializeField]
     private Transform groundDetection;
     
@@ -26,23 +31,26 @@ public class EnemyController : MonoBehaviour
     private Transform playerTransform;
     private Renderer enemyRenderer;
     private Animator enemyKnightLegsAnimator;
-    private GameMaster gm;
+    private GameMaster gameMaster;
 
     private void Start()
     {
         enemyRenderer = gameObject.GetComponent<Renderer>();
         playerTransform = GameObject.Find("Player").GetComponent<Transform>();
-        gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        gameMaster = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
     }
 
     private void Update()
     {
-        if (!gm.isPaused)
+        //make sure game isn't paused
+        if (!gameMaster.isPaused)
         {
+            //if it's a regular enemy
             if (gameObject.tag == "Enemy")
             {
                 NormalEnemyBehavior();
             }
+            //if it's a knight enemy
             else if (gameObject.tag == "Knight Enemy")
             {
                 enemyKnightLegsAnimator = GetComponentInChildren<Animator>();
@@ -51,11 +59,18 @@ public class EnemyController : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Behavior for enemy that is not a knight
+    /// </summary>
     void NormalEnemyBehavior()
     {
+        //move the enemy
         transform.Translate(Vector2.left * speed * Time.deltaTime);
 
-        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
+        //detect the ground and platform distance
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, platformDistance);
+
+        //change sprite direction based on movement direction
         if (groundInfo.collider == false)
         {
             if (movingLeft == true)
@@ -71,17 +86,29 @@ public class EnemyController : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Behavior for enemy knight
+    /// </summary>
     void KnightEnemyBehavior()
     {
+        //get the distance between the enemy and the player
         float distanceFromEnemy = Mathf.Abs(gameObject.transform.position.x - playerTransform.position.x);
 
+        //if the knight is not in the camera and the player is too far from the enemy
         if (!enemyRenderer.isVisible || distanceFromEnemy > detectDistance)
         {
             playerDetected = false;
+            
+            //keep the enemy knight walking (animation)
             enemyKnightLegsAnimator.SetBool(enemyKnightWalking, true);
+
+            //move the enemy
             transform.Translate(Vector2.left * speed * Time.deltaTime);
 
-            RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
+            //detect the ground and platform distance
+            RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, platformDistance);
+
+            //change sprite direction based on movement direction
             if (groundInfo.collider == false)
             {
                 if (movingLeft == true)
@@ -96,10 +123,14 @@ public class EnemyController : MonoBehaviour
                 }
             }
         }
+
+        //if the knight is in the camera and the player is close enough to the enemy
         else if (enemyRenderer.isVisible && distanceFromEnemy <= detectDistance)
         {
+            //stop the knight walking (animation)
             enemyKnightLegsAnimator.SetBool(enemyKnightWalking, false);
 
+            //change sprite direction based on player position
             if (playerTransform.position.x > gameObject.transform.position.x && !facingRight)
             {
                 transform.Rotate(0f, 180f, 0);
@@ -110,6 +141,8 @@ public class EnemyController : MonoBehaviour
                 transform.Rotate(0f, 180f, 0);
                 facingRight = false;
             }
+
+            //player is detected
             playerDetected = true;
         }
         
